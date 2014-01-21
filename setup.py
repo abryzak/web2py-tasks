93852b0c15c2e6d4ae2352a1571b6ed19b90daf0
Adam Bryzak <adam@bryzak.net>
2014-01-21 17:56:47 +1000

    Fix exported module

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='web2py-tasks',
-      version='1.1',
+      version='1.2',
       author='Adam Bryzak',
       author_email='adam@bryzak.net',
-      py_modules=['json_sync'],
+      py_modules=['task_decorator'],
      )
